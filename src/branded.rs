f85e6a1f1d70f514388e0b3ff2cc1b0ee824271b
yvt <i@yvt.jp>
2021-08-30 23:22:25 +0900

    doc: add examples to `with_branded_token`

--- src/branded.rs
+++ src/branded.rs
@@ -18,6 +18,8 @@ pub struct BrandedTag<'brand> {
 /// Lifetime-branded token that can be used to access the contents of a
 /// `TokenLock<T, `[`BrandedTokenId`]`<'brand>>`.
 /// Created by [`with_branded_token`].
+///
+/// See [`with_branded_token`] for an example.
 pub type BrandedToken<'brand> = SingletonToken<BrandedTag<'brand>>;
 
 /// Zero-sized logical equivalent of `&'a `[`BrandedToken`]`<'brand>`.
@@ -60,6 +62,55 @@ pub type UnsyncBrandedTokenLock<'brand, T> = UnsyncTokenLock<T, BrandedTokenId<'
 /// soundness (excluding the extensions provided by this crate).
 ///
 /// [1]: http://plv.mpi-sws.org/rustbelt/ghostcell/
+///
+/// # Example
+///
+/// ```rust,edition2018
+/// use rayon::prelude::*;
+/// use tokenlock::{with_branded_token, BrandedTokenLock};
+///
+/// struct Node<'arena, 'brand> {
+///     next: BrandedTokenLock<'brand, Option<&'arena Node<'arena, 'brand>>>,
+/// }
+///
+/// with_branded_token(|mut token| {
+///     let arena = [
+///         Node { next: BrandedTokenLock::wrap(None) },
+///         Node { next: BrandedTokenLock::wrap(None) },
+///     ];
+///
+///     // Link the nodes together
+///     arena[0].next.replace(&mut token, Some(&arena[1]));
+///     arena[1].next.replace(&mut token, Some(&arena[0]));
+///
+///     // Unlike `Cell`, this doesn't prevent shared read-only access
+///     arena.par_iter().for_each(|item| {
+///         println!("{:p} is linked to {:p}", item, item.next.get(&token).unwrap());
+///     });
+/// });
+/// ```
+///
+/// ```rust,edition2018
+/// use rayon::prelude::*;
+/// use std::sync::mpsc;
+/// use tokenlock::{with_branded_token, UnsyncBrandedTokenLock};
+///
+/// with_branded_token(|mut token| {
+///     let unsync_token = token.borrow_unsync_mut();
+///
+///     let senders: Vec<_> = (0..4)
+///         .map(|_| UnsyncBrandedTokenLock::wrap(mpsc::channel().0))
+///         .collect();
+///
+///     // `unsync_token` unlocks `UnsyncBrandedTokenLock`
+///     let _ = senders[0].read(&*unsync_token).send(());
+///
+///     // `Sender` is `!Sync`, but that doesn't make `senders` `!Sync` because
+///     // `UnsyncBrandedTokenLock` can only be borrowed by one thread at the
+///     // same time
+///     senders.par_iter().for_each(|item| println!("{:p}", item));
+/// });
+/// ```
 #[inline]
 pub fn with_branded_token<R>(f: impl for<'brand> FnOnce(BrandedToken<'brand>) -> R) -> R {
     // Safety: As `f` is generic over `'brand`, and `BrandedToken::
